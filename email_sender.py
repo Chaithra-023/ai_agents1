e6071727fb7c72b9b89cee24671baa85335815ad
Chaithra-023 <you@chaithrachaitu468@gmail.com>
2026-02-10 10:37:04 +0530

    secrets1

--- email_sender.py
+++ email_sender.py
@@ -2,21 +2,22 @@ import smtplib
 from email.message import EmailMessage
 from secrets import sender_email, receiver_email, app_password
 
+def send_email(receiver_email,content):
 
-sender_email="4mh23cs023@gmail.com"
-receiver_email="4mh23cs023@gmail.com"
-app_password="zico huiu ajlb qctc"
 
 # Create the email
-msg = EmailMessage()
-msg["From"] = sender_email
-msg["To"] = receiver_email
-msg["Subject"] = "Hello from Python"
-msg.set_content("This email was sent using Python 🐍")
+    msg = EmailMessage()
+    msg["From"] = sender_email
+    msg["To"] = receiver_email
+    msg["Subject"] = "Hello from Python"
+    msg.set_content(content)
 
-# Send it
-with smtplib.SMTP("smtp.gmail.com", 587) as server:
-    server.starttls()                  # Secure the connection
-    server.login(sender_email, app_password)
-    server.send_message(msg)
-print("Email sent successfully")
+    # Send it
+    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+        server.starttls()                  # Secure the connection
+        server.login(sender_email, app_password)
+        server.send_message(msg)
+    
+    print("Email sent successfully")
+
+send_email(receiver_email="4mh23cs023@gmail.com",subject="Hello from python",content="This is email was sent using python")   
