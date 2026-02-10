26f2c6bfa7d404922971ee154f41835d7ea0ad40
Chaithra-023 <you@chaithrachaitu468@gmail.com>
2026-02-10 11:38:06 +0530

    Email

--- email_sender.py
+++ email_sender.py
@@ -2,14 +2,14 @@ import smtplib
 from email.message import EmailMessage
 from secrets import sender_email, receiver_email, app_password
 
-def send_email(receiver_email,content):
-
+def send_email(receiver_email:str,subject:str,content:str) -> str:
+    """send an email to the receiver with the given subject and content"""
 
 # Create the email
     msg = EmailMessage()
     msg["From"] = sender_email
     msg["To"] = receiver_email
-    msg["Subject"] = "Hello from Python"
+    msg["Subject"] = subject
     msg.set_content(content)
 
     # Send it
@@ -20,4 +20,5 @@ def send_email(receiver_email,content):
     
     print("Email sent successfully")
 
-send_email(receiver_email="4mh23cs023@gmail.com",subject="Hello from python",content="This is email was sent using python")   
+if __name__ == "__main__":
+    send_email(receiver_email="4mh23cs023@gmail.com",subject="Hello from python",content="This is email was sent using python")   
--- main2.py
+++ main2.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 load_dotenv()
+from email_sender import send_email
 
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.agents import create_agent
@@ -25,13 +26,13 @@ model = ChatGoogleGenerativeAI(
 
 agent = create_agent(
     model=model,
-    tools=[get_weather,isprime],
+    tools=[get_weather,isprime,send_email],
     system_prompt="You are a helpful assistant",
 )
 
 # Run the agent
 response = agent.invoke(
-    {"messages": [{"role": "user", "content": "what is the weather in mysore and is 2001 a prime number?"}]}
+    {"messages": [{"role": "user", "content": "what is the weather in mysore and is 2001 a prime number,send email to 4mh23cs023@gmail.com with the content about weather and result of prime number?"}]}
 )
 
 # Print only the final AI response
